85e715689dbba06f363ab7b6367c4c136bdca6e2
byt3r <glevin@gmail.com>
2019-08-03 21:14:41 +0300

    added stuff

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,16 @@
+/* eslint-disable no-undef */
 var express = require('express');
 var router = express.Router();
+var networkFetch = require('../lib/networkFetcher').networkFetch;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/data', async function(req, res, next) {
+  res.send(await networkFetch())
+});
+
 
 module.exports = router;
